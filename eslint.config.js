4791418151b80478cb30f479e00f6e6b06d0fe30
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-05-11 17:42:29 -0400

    hmm

--- eslint.config.js
+++ eslint.config.js
@@ -1,26 +1,12 @@
-import { join } from "node:path";
-
 import { configs } from "@nullvoxpopuli/eslint-configs";
 
 const defaults = configs.ember(import.meta.dirname);
-
-const tsconfig = join(import.meta.dirname, "config/tsconfig.tests.json");
 
 const config = [
   {
     ignores: ["dist", "node_modules", "declarations", "dist-app", "dist-docs", "dist-tests"],
   },
   ...defaults,
-  ...defaults.map((x) => {
-    if (x.languageOptions?.parserOptions?.tsconfigRootDir) {
-      x.languageOptions.parserOptions.project = tsconfig;
-      delete x.languageOptions.parserOptions.projectService;
-      x.rules ||= {};
-      x.rules["@typescript-eslint/consistent-type-imports"] = "error";
-    }
-
-    return x;
-  }),
 ];
 
 export default config;
